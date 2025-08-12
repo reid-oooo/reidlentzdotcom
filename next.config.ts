0aafde3ba8a966c470ea00815dff1461283b20cd
Reid <lentz733@gmail.com>
2025-08-12 09:54:06 -0400

    furthered build error resolution for npm critters

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
